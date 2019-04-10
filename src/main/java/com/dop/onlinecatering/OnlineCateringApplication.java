de5369fa465b7f6a81092ead679f2c625ef05204
Amir <cnooed@gmail.com>
2019-04-10 23:33:23 +0430

    initial commit

---
+++ src/main/java/com/dop/onlinecatering/OnlineCateringApplication.java
@@ -0,0 +1,13 @@
+package com.dop.onlinecatering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCateringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineCateringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dop/onlinecatering/OnlineCateringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dop.onlinecatering;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineCateringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

d252238cc9dfc657893b9cbd98937efd4c9dd993
Amir <cnooed@gmail.com>
2019-04-11 11:24:11 -0400

    added some test data to data base

---
+++ src/main/java/com/dop/onlinecatering/bootstrap/DbSeeder.java
@@ -0,0 +1,36 @@
+package com.dop.onlinecatering.bootstrap;
+
+import com.dop.onlinecatering.model.User;
+import com.dop.onlinecatering.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class DbSeeder implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+
+    public DbSeeder(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        initData();
+    }
+
+    void initData(){
+
+        User user = new User();
+
+        user.setFirstName("aaa");
+        user.setFirstName("bbb");
+        user.setPhoneNumber("444444444");
+        user.setCreated(new Date());
+        user.setUpdated(new Date());
+        user.setVerified(true);
+        userRepository.save(user);
+    }
+}
